d66be2eb3a80c5542b5644665581c7b85695e843
Matheus Cavasini <cavasini.dev@gmail.com>
2025-10-18 21:50:00 -0300

    Inserindo Autentificação no ProfileAnalyzer

---
+++ ProfileAnalyzerService/src/main/java/com/fiap/challenge/ProfileAnalyzerService/security/JwtFilter.java
@@ -0,0 +1,79 @@
+package com.fiap.challenge.ProfileAnalyzerService.security;
+
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import io.jsonwebtoken.Jwts;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import com.auth0.jwt.algorithms.Algorithm;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.PublicKey;
+import java.security.interfaces.RSAPublicKey;
+import java.util.Collections;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+
+
+    private final Algorithm algorithm;
+
+    public JwtFilter(@Value("${jwt.secret}") String secretKey) {
+        this.algorithm = Algorithm.HMAC256(secretKey.getBytes(StandardCharsets.UTF_8));
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var token = this.recoverToken(request);
+        if (token != null) {
+            try {
+                var login = JWT.require(algorithm)
+                        .withIssuer("auth-api")
+                        .build()
+                        .verify(token)
+                        .getSubject();
+
+                var authentication = new UsernamePasswordAuthenticationToken(login, null, Collections.emptyList());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            } catch (JWTVerificationException exception) {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                response.getWriter().write("Token ausente");
+                return;
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request) {
+        var authHeader = request.getHeader("Authorization");
+
+        if (authHeader == null) return null;
+        return authHeader.replace("Bearer ", "");
+    }
+
+
+    public String validateToken(String token) {
+        try {
+            JWT.require(algorithm)
+                    .withIssuer("auth-api")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+
+        } catch (JWTVerificationException exception) {
+            return "";
+        }
+        return "";
+    }
+
+}
